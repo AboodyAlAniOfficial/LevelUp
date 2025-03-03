23244973ecd3acfc9f21582bee81b5c3fd032d51
Adrien Hopkins <adrien.p.hopkins@gmail.com>
2025-03-03 10:52:03 -0500

    Create barebones login UI

---
+++ interface/levelup_vue/levelup_vue/src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Login</h1>
+    <form action="submit">
+        <label for="username">Username:</label>
+        <input id="username">
+        <br>
+        <label for="password">Password:</label>
+        <input id="password">
+        <br>
+        <input type="submit" value="Login">
+    </form>
+</template>
