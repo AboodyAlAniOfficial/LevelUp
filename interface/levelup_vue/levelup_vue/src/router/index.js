d0751624fb61f30bec01beafd4b7fa4e3223c78a
khoaTranT <92763281+khoaTranT@users.noreply.github.com>
2025-03-07 13:24:27 -0500

    Update index.js

--- interface/levelup_vue/levelup_vue/src/router/index.js
+++ interface/levelup_vue/levelup_vue/src/router/index.js
@@ -14,12 +14,40 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/ProfileView.vue')
+  },
+  {
+    path: '/meal',
+    name: 'meal',
+    component: () => import('../views/MealView.vue')
+  },
+  {
+    path: '/exercise',
+    name: 'exercise',
+    component: () => import('../views/ExerciseView.vue')
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
 })
 
 export default router
