b07fba176f5959ec49c2dc26a6a0ec97c73bb672
khoaTranT <92763281+khoaTranT@users.noreply.github.com>
2025-03-09 14:53:45 -0400

    Update views

--- interface/levelup_vue/levelup_vue/src/views/MealView.vue
+++ interface/levelup_vue/levelup_vue/src/views/MealView.vue
@@ -7,6 +7,27 @@
 <script>
 export default {
   name: 'meal',
-  components: {}
+  data() {
+    return {
+      meal: []
+    }
+  },
+  components: {
+    mounted() {
+      this.getMealList()
+    }
+  },
+  methods: {
+    getMealList() { //STUB: Need to update to match component app in django. Fetches the entire list of food from database, need another method for fetching users'.
+      axios
+        .get('api/v1/meals') //Name of the django page storing these data
+        .then(response=>{
+          this.profile = response.data
+        })
+        .catch(error=>{
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
--- interface/levelup_vue/levelup_vue/src/views/ProfileView.vue
+++ interface/levelup_vue/levelup_vue/src/views/ProfileView.vue
@@ -5,8 +5,31 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'ProfileView',
-  components: {}
+  data() {
+    return {
+      userProfile: []
+    }
+  },
+  components: {
+    mounted() {
+      this.getUserProfile() 
+    }
+  },
+  methods: {
+    getUserProfile() { //STUB: Need to update to match component app in django
+      axios
+        .get('api/v1/u/profile') //Name of the django page storing these data
+        .then(response=>{
+          this.profile = response.data
+        })
+        .catch(error=>{
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
