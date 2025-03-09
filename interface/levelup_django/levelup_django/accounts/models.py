251d6861180d28bea1d2a870d66dee0f40c09815
Adrien Hopkins <adrien.p.hopkins@gmail.com>
2025-03-09 18:01:06 -0500

    Add units model

--- interface/levelup_django/levelup_django/accounts/models.py
+++ interface/levelup_django/levelup_django/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Unit(models.Model):
+    username = models.CharField(max_length=150)
+    length = models.CharField(max_length=16)
+    mass = models.CharField(max_length=16)
+    energy = models.CharField(max_length=16)
