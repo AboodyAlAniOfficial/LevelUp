e782d0beea1a595e81efb7e2ac6e1b7e989f3b8f
BilalJameel665 <44616769+BilalJameel665@users.noreply.github.com>
2025-04-06 13:39:48 -0400

    Deleted useless FollowerService directory

--- FollowerService/followerModel.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-#the user instance will have to be obtained through an Api call 
-
-class Follower(models.Model):
-    user_id = models.IntegerField()  # get the ID of the user who is searching for a user to follow
-    followerUserId = models.IntegerField()  # get the ID of the user being followed
-
-    class Meta:
-        unique_together = ('user_id', 'followerUserId')  # Prevent duplicate follows
